7c80db0cf32bc79a514336e2fcc349bed98ac688
ReactiveX22 <akibreactive@gmail.com>
2024-04-06 07:16:13 +0600

    Button, auth, db

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+import { cx } from 'class-variance-authority';
+import { ClassValue } from 'class-variance-authority/types';
+
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(cx(inputs));
+}
